72167cf5d41924a064917120050988c21aa398e3
bhiles <bennetthiles@gmail.com>
2015-01-19 21:46:40 -0500

    create two new tables

---
+++ db/01-create-tables.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE p_user (
+       id         bigserial PRIMARY KEY,
+       email      varchar(120) NOT NULL,
+       firstName  varchar(120) NOT NULL,
+       lastName   varchar(120) NOT NULL,
+       points     bigint DEFAULT 0
+);
+
+CREATE TABLE tx (
+       p_user_id bigint NOT NULL,
+       amount    bigint NOT NULL,
+       type      varchar(6) 
+);
