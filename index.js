63347b8760cf0408d967532915d5af5dffd314ce
bhiles <bennetthiles@gmail.com>
2015-01-20 00:10:55 -0500

    remove unnecessary pieces

--- index.js
+++ index.js
@@ -15,20 +15,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // Misc routes
 
 app.get('/', function(request, response) {
-  response.send('Hello World! and Bennett!');
+  response.send('View the API at https://github.com/bhiles/loyalty#api');
 });
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
 
 // Helper functions
 
